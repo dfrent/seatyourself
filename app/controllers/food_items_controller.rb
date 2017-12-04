4b8d3acdf7b7add875c50496a5f6aabe1da5134e
cordell <cordell.perry@gmail.com>
2017-12-04 15:27:45 -0500

    controller for users, restaurants, reservatons, food items

---
+++ app/controllers/food_items_controller.rb
@@ -0,0 +1,27 @@
+class FoodItemsController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def new
+
+  end
+
+  def destroy
+
+  end
+
+end
---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,29 @@
+class ReservationsController < ApplicationController
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def new
+
+  end
+
+  def destroy
+
+  end
+
+
+
+end
