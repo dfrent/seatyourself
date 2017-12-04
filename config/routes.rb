49c687f96f87c34498a5898464281cc8d07d0b31
somethinrother <jamesneilson4@gmail.com>
2017-12-04 16:54:48 -0500

    Add session route and change resources to resource for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, except: [:index]
+  resource :users, except: [:index]
   resources :restaurants do
     resources :food_items, except: [:show]
   end
   resources :reservations, except: [:index]
+
+  resource  :sessions, only: %i(create new destroy)
 
 end
