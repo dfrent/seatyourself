33d04ef04f0c0604dda807351459fd04cb64ff9e
somethinrother <jamesneilson4@gmail.com>
2017-12-04 16:23:09 -0500

    Routes and directory edits: all views working

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, :except => [:index]
+  resources :users, except: [:index]
   resources :restaurants do
-    resources :food_items, :except => [:show]
+    resources :food_items, except: [:show]
   end
-  resources :reservations, :except => [:index]
-
-
+  resources :reservations, except: [:index]
 
 end
