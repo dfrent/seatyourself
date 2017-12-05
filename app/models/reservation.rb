d2a4aeaae7537c7c5f520f36f03d794f9b024c74
KYLE LIPTON <lipton.kyle@gmail.com>
2017-12-04 21:35:13 -0500

    reservation validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,16 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
 
-  validates :time, presence: true
-  validates :size, numericality: { message: "%{value} seems wrong. Please enter a number..." }
+  validates :time, :size, presence: true
+  validates :size, numericality: { message: "%{value} seems wrong. Please enter a number..." }, unless: :restaurant_at_capacity?
+
+
+private
+    def restaurant_at_capacity?
+      puts self.restaurant
+      if self.size > self.restaurant.capacity
+        puts "Restaurant is at capacity"
+      end
+    end
 
 end
