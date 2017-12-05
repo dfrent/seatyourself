38f5202989481272bed8554ee70c9b986a2001f1
KYLE LIPTON <lipton.kyle@gmail.com>
2017-12-05 13:21:31 -0500

    changed user table password column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205172738) do
+ActiveRecord::Schema.define(version: 20171205181542) do
 
   create_table "food_items", force: :cascade do |t|
     t.integer  "restaurant_id"
@@ -53,10 +53,10 @@ ActiveRecord::Schema.define(version: 20171205172738) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "authority"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
