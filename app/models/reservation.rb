e59b461c19baceb8783ebff5d0d67b3a04372f5f
KYLE LIPTON <lipton.kyle@gmail.com>
2017-12-04 20:18:41 -0500

    validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :user
-  belongs_to :restaurant 
+  belongs_to :restaurant
+
+  validates :time, presence: true
+  validates :size, numericality: { message: "%{value} seems wrong. Please enter a number..." }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
 
   has_and_belongs_to_many :restaurants
   has_many :reservations
+
+  validates :name, :password, :email, presence: true
+  validates :email, uniqueness: {message: "%{value} is already taken. Please enter a new email"}
+
 end
