4bc6d359dede17619d22d3ee63f2d5695bc7e47d
somethinrother <jamesneilson4@gmail.com>
2017-12-05 10:25:48 -0500

    Add default authority value of customer to new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
+  before_save :default_values
 
   has_and_belongs_to_many :restaurants
   has_many :reservations
 
   validates :name, :password, :email, presence: true
   validates :email, uniqueness: {message: "%{value} is already taken. Please enter a new email"}
+
+
+  def default_values
+    self.authority ||= 'customer'
+  end
 
 end
