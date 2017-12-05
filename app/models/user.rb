5be3afcaf45264ccab42108c142689a5013da75d
KYLE LIPTON <lipton.kyle@gmail.com>
2017-12-05 13:10:03 -0500

    secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
+
+  has_secure_password
+
   before_save :default_values
 
   has_and_belongs_to_many :restaurants
   has_many :reservations
 
+  validates :name, presence: true
   validates :name, :password, :email, presence: true
   validates :email, uniqueness: {message: "%{value} is already taken. Please enter a new email"}
 
