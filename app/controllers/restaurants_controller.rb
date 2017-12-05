5d1ffe79ca7a4261d3e0ef3a5d04852ca9ad279f
cordell <cordell.perry@gmail.com>
2017-12-05 11:45:02 -0500

    restaurant controller, testing and debug

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,27 +5,44 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-
-  end
-
-  def create
-
-  end
-
-  def edit
-
-  end
-
-  def update
-
+    @restaurants = Restaurant.find(params[id:])
+    
   end
 
   def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new
+    @restaurant.name = params[:restaurant][:name]
+    @restaurant.location = params[:restaurant][:location]
+    @restaurant.capacity = params[:restaurant][:capacity]
+    @restaurant.open = params[:restaurant][:open]
+    @restaurant.close = params[:restaurant][:close]
+  end
+
+
+  def edit
+    @restaurants = Restaurant.find(params[id:])
+  end
+
+  def update
+    @restaurants = Restaurant.find(params[id:])
+    @restaurant.name = params[:restaurant][:name]
+    @restaurant.location = params[:restaurant][:location]
+    @restaurant.capacity = params[:restaurant][:capacity]
+    @restaurant.open = params[:restaurant][:open]
+    @restaurant.close = params[:restaurant][:close]
 
   end
 
-  def delete
 
+  def delete
+    @restaurants = Restaurant.find(params[id:])
+    @restaurant.destroy
+    flash[:notice] = "you have successfully deleted a restaurant"
+    redirect_to restaurant_url
   end
 
 
