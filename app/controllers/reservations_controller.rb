7fc4d3b58b3890cbd102547b808bf51c2895d1ab
cordell <cordell.perry@gmail.com>
2017-12-06 19:50:09 -0500

    destroy and edit functions for reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -38,18 +38,33 @@ class ReservationsController < ApplicationController
   end
 
   def update
+    @restaurant = Restaurant.find(params[:id])
+    @reservation.time = params[:reservation][:time]
+    @reservation.date = params[:reservation][:date]
+    @reservation.size = params[:reservation][:size]
+    @reservation.max_reservation_size = params[:reservation][:max_reservation_size]
 
+    if @reservation.save
+      flash[:success] = "#{@reservation.name} has been successfully updated"
+      redirect_to restaurant_path
+    else
+      flash.now[:alert] = "Sorry, there were issues updating your reservation"
+      render 'edit'
+    end
   end
 
   def new
     @reservation = Reservation.new
-
     @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def destroy
-
+    @restaurant = Restaurant.find(params[:id])
+    @reservation.destroy
+    flash[:notice] = "You have successfully deleted a reservation"
+    redirect_to restaurant_url
   end
+
  def ensure_logged_in_for_reservation
    unless current_user
      flash[:alert] = "You must be logged in to make a reservation"
