3d06609a6fb45fc4de994a84466a53f8f7637be3
somethinrother <jamesneilson4@gmail.com>
2017-12-10 00:13:34 -0500

    Create method to list all available time slots

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,9 +8,6 @@ class Restaurant < ApplicationRecord
   validates :name, :location, :capacity, :open_time, :close_time, :max_reservation_size, presence: true
   validates :max_reservation_size, numericality: { message: "%{value} seems wrong. Please enter a number." }
   validates :max_reservation_size, numericality: { :greater_than_or_equal_to => 1, message: " can't be negative." }
-  validate
-  # validates :open_time, numericality: { :greater_than_or_equal_to => 1, message: "can't be below 1." }
-  # validates :close_time, numericality: { :less_than => 25, message: " can't be above 24." }
 
   def time_display(restaurant_time)
     if restaurant_time.in_time_zone("UTC").hour == 0
@@ -24,48 +21,25 @@ class Restaurant < ApplicationRecord
     end
   end
 
-  def correct_day_in_date
-    if close_time < open_time
-      self.close_time = self.close_time + 1.day
-    end
-  end
+  def available_times
+    first_slot = open_time.hour
+    last_slot  = close_time.hour
 
-  # def available_times
-  #   time_spent_open = (close_time - open_time)
-  #   first_time = open_time
-  #   time_list = []
-  #
-  #   (open_time..close_time - 1).to_a.each do |hour|
-  #     time_list << hour
-  #     time_list << hour + 0.5
-  #   end
-  #
-  #   time_list.map! do |time_slot|
-  #
-  #     if time_slot.class == Float
-  #       if time_slot == 0.5 || time_slot == 24.5
-  #         time_slot = "12:30 AM"
-  #       elsif time_slot == 12.5
-  #         time_slot = "12:30 PM"
-  #       elsif time_slot >= 13
-  #         time_slot = "#{time_slot.to_i - 12}:30 PM"
-  #       else
-  #         time_slot = "#{time_slot.to_i}:30 AM"
-  #       end
-  #     elsif time_slot.class == Integer
-  #       if time_slot == 0 || time_slot == 24
-  #         time_slot = "12 AM"
-  #       elsif time_slot == 12
-  #         time_slot = "12 PM"
-  #       elsif time_slot >= 13
-  #         time_slot = "#{time_slot - 12} PM"
-  #       else
-  #         time_slot = "#{time_slot} AM"
-  #       end
-  #     end
-  #   end
-  #   time_list
-  # end
+    if last_slot < first_slot
+      last_slot += 24
+    end
+
+    hours_open = last_slot - first_slot
+    reservation_slots = []
+    hour = 0
+
+    hours_open.times do
+      reservation_slots << open_time + hour.hours
+      reservation_slots << open_time + (hour + 0.5).hours
+      hour += 1
+    end
+    reservation_slots
+  end
 
   def price_range
     price_array = food_items.sort_by {|item| item.price}
