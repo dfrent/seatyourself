20fe95581127e6ff6fb9fc279ee7c5ec8feb5867
somethinrother <jamesneilson4@gmail.com>
2017-12-10 00:54:34 -0500

    Validate that reservation is not before opening hours

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,7 +6,7 @@ class Reservation < ApplicationRecord
   # validate  :in_the_past?
   # validate  :reservation_too_large?
   # validate  :after_close?
-  # validate  :before_open?
+  validate  :before_open?
   validates :date, :size, presence: true
   validates :size, numericality: { message: "%{value} seems wrong. Please enter a number." }
   validates :size, numericality: { :greater_than_or_equal_to => 1, message: "of reservation can't be negative." }
@@ -38,16 +38,16 @@ class Reservation < ApplicationRecord
   end
 
   # def after_close?
-  #   if self.time.to_i > restaurant.close_time
-  #     errors.add(:time, "of reservation cannot be after closing hours.")
+  #   if self.date > restaurant.close_time
+  #     errors.add(:date, "of reservation cannot be after closing hours.")
   #   end
   # end
   #
-  # def before_open?
-  #   if self.time.to_i < restaurant.open_time
-  #     errors.add(:time, "of reservation cannot be before opening hours.")
-  #   end
-  # end
+  def before_open?
+    if self.date.hour < restaurant.open_time.hour
+      errors.add(:date, "of reservation cannot be before opening hours.")
+    end
+  end
   #
   # def in_the_past?
   #   if self.date < Date.today
