c4c3e40a77fd00eeba60609c77cb2976571656dd
somethinrother <jamesneilson4@gmail.com>
2017-12-09 22:18:53 -0500

    Prep seatyourself for shift to datetime instead of integer

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -11,20 +11,20 @@ class Reservation < ApplicationRecord
   validates :size, numericality: { message: "%{value} seems wrong. Please enter a number." }
   validates :size, numericality: { :greater_than_or_equal_to => 1, message: "of reservation can't be negative." }
 
-  def room_in_restaurant?
-    current_bookings = 0
-    restaurant.reservations.where(time: "#{self.time}").each do |reservation|
-      current_bookings += reservation.size
-    end
-
-    current_capacity = restaurant.capacity - current_bookings
-
-    if size <= current_capacity
-      true
-    else
-      errors.add(:size, "of reservation is too large. Apologies!")
-    end
-  end
+  # def room_in_restaurant?
+  #   current_bookings = 0
+  #   restaurant.reservations.where(time: "#{self.time}").each do |reservation|
+  #     current_bookings += reservation.size
+  #   end
+  #
+  #   current_capacity = restaurant.capacity - current_bookings
+  #
+  #   if size <= current_capacity
+  #     true
+  #   else
+  #     errors.add(:size, "of reservation is too large. Apologies!")
+  #   end
+  # end
 
   def reservation_too_large?
     if size > restaurant.max_reservation_size
@@ -43,11 +43,11 @@ class Reservation < ApplicationRecord
   #     errors.add(:time, "of reservation cannot be before opening hours.")
   #   end
   # end
-
-  def in_the_past?
-    if self.date < Date.today
-      errors.add(:date, "of reservation cannot be in the past.")
-    end
-  end
+  #
+  # def in_the_past?
+  #   if self.date < Date.today
+  #     errors.add(:date, "of reservation cannot be in the past.")
+  #   end
+  # end
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,45 +8,45 @@ class Restaurant < ApplicationRecord
   validates :name, :location, :capacity, :open_time, :close_time, :max_reservation_size, presence: true
   validates :max_reservation_size, numericality: { message: "%{value} seems wrong. Please enter a number." }
   validates :max_reservation_size, numericality: { :greater_than_or_equal_to => 1, message: " can't be negative." }
-  validates :open_time, numericality: { :greater_than_or_equal_to => 1, message: "can't be below 1." }
-  validates :close_time, numericality: { :less_than => 25, message: " can't be above 24." }
+  # validates :open_time, numericality: { :greater_than_or_equal_to => 1, message: "can't be below 1." }
+  # validates :close_time, numericality: { :less_than => 25, message: " can't be above 24." }
 
-  def available_times
-    time_spent_open = (close_time - open_time)
-    first_time = open_time
-    time_list = []
-
-    (open_time..close_time - 1).to_a.each do |hour|
-      time_list << hour
-      time_list << hour + 0.5
-    end
-
-    time_list.map! do |time_slot|
-
-      if time_slot.class == Float
-        if time_slot == 0.5 || time_slot == 24.5
-          time_slot = "12:30 AM"
-        elsif time_slot == 12.5
-          time_slot = "12:30 PM"
-        elsif time_slot >= 13
-          time_slot = "#{time_slot.to_i - 12}:30 PM"
-        else
-          time_slot = "#{time_slot.to_i}:30 AM"
-        end
-      elsif time_slot.class == Integer
-        if time_slot == 0 || time_slot == 24
-          time_slot = "12 AM"
-        elsif time_slot == 12
-          time_slot = "12 PM"
-        elsif time_slot >= 13
-          time_slot = "#{time_slot - 12} PM"
-        else
-          time_slot = "#{time_slot} AM"
-        end
-      end
-    end
-    time_list
-  end
+  # def available_times
+  #   time_spent_open = (close_time - open_time)
+  #   first_time = open_time
+  #   time_list = []
+  #
+  #   (open_time..close_time - 1).to_a.each do |hour|
+  #     time_list << hour
+  #     time_list << hour + 0.5
+  #   end
+  #
+  #   time_list.map! do |time_slot|
+  #
+  #     if time_slot.class == Float
+  #       if time_slot == 0.5 || time_slot == 24.5
+  #         time_slot = "12:30 AM"
+  #       elsif time_slot == 12.5
+  #         time_slot = "12:30 PM"
+  #       elsif time_slot >= 13
+  #         time_slot = "#{time_slot.to_i - 12}:30 PM"
+  #       else
+  #         time_slot = "#{time_slot.to_i}:30 AM"
+  #       end
+  #     elsif time_slot.class == Integer
+  #       if time_slot == 0 || time_slot == 24
+  #         time_slot = "12 AM"
+  #       elsif time_slot == 12
+  #         time_slot = "12 PM"
+  #       elsif time_slot >= 13
+  #         time_slot = "#{time_slot - 12} PM"
+  #       else
+  #         time_slot = "#{time_slot} AM"
+  #       end
+  #     end
+  #   end
+  #   time_list
+  # end
 
   def price_range
     price_array = food_items.sort_by {|item| item.price}
